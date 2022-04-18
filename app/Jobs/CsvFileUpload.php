d2ecee1292e71c62be2c855c1e72cceea2c8eb29
premahp <ppanwar@ahpservicing.com>
2022-04-18 22:24:46 +0530

    Added ids to another tables

--- app/Jobs/CsvFileUpload.php
+++ app/Jobs/CsvFileUpload.php
@@ -53,15 +53,30 @@ class CsvFileUpload implements ShouldQueue
                         $address['zip'] = $csvValue[8];
                         
                         // Storing the data in the DB for now.
-                        DB::table('employee')->insert([
-                                $employee
-                            ]);
-                        DB::table('organization')->insert([
+                        $organization = DB::table('organization')->insert([
                             $organization
                         ]);
-                        DB::table('address')->insert([
+                        $organizationId = DB::getPdo()->lastInsertId();
+
+                        $employee['organizationId'] = $organizationId;
+
+                        $employe = DB::table('employee')->insert([
+                                $employee
+                            ]);
+                        $employeId = DB::getPdo()->lastInsertId();
+                        
+                        $address = DB::table('address')->insert([
                             $address
                         ]);
+                        $addressId = DB::getPdo()->lastInsertId();
+
+                        // Preparing data for employe address table
+                        $organizationAddressData['employeeId'] = $employeId;
+                        $organizationAddressData['addressId'] = $addressId;
+                        $organizationAddress = DB::table('employee_address')->insert([
+                            $organizationAddressData
+                        ]);
+
                     }
                 }
             }
